60800048061aca879014f8e929b48758a1f9b818
NyanSilence <nyansilence@gmail.com>
2019-07-04 17:51:20 +0700

    update

--- content/dam/isetan_mitsukoshi/advertise/shinjuku/natsuyasumi/201907/js/script.js
+++ content/dam/isetan_mitsukoshi/advertise/shinjuku/natsuyasumi/201907/js/script.js
@@ -1,11 +1,22 @@
-if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
-    $('body').on("mousewheel", function () {
-        // remove default behavior
-        event.preventDefault(); 
+// if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+//     $('body').on("mousewheel", function () {
+//         // remove default behavior
+//         event.preventDefault(); 
 
-        //scroll without smoothing
-        var wheelDelta = event.wheelDelta;
-        var currentScrollPosition = window.pageYOffset;
-        window.scrollTo(0, currentScrollPosition - wheelDelta);
-    });
-}
+//         //scroll without smoothing
+//         var wheelDelta = event.wheelDelta;
+//         var currentScrollPosition = window.pageYOffset;
+//         window.scrollTo(0, currentScrollPosition - wheelDelta);
+//     });
+// }
+
+// $(document).ready(function(){
+//     var $top
+//     var $height
+//     // if($('.mod-banner')) {
+//     $top = $('.mod-banner').offset().top
+//     $height = $('.mod-banner').height()
+//     // }
+
+//     $('.bg-background').css({top: $top, height: $height})
+// })
