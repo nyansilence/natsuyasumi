cad03b89ec62d4ff9d94a3def553e36e6c68a3ad
NyanSilence <nyansilence@gmail.com>
2019-07-04 18:02:27 +0700

    update js

--- content/dam/isetan_mitsukoshi/advertise/shinjuku/natsuyasumi/201907/js/script.js
+++ content/dam/isetan_mitsukoshi/advertise/shinjuku/natsuyasumi/201907/js/script.js
@@ -1,14 +1,14 @@
-// if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
-//     $('body').on("mousewheel", function () {
-//         // remove default behavior
-//         event.preventDefault(); 
+if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+    $('body').on("mousewheel", function () {
+        // remove default behavior
+        event.preventDefault(); 
 
-//         //scroll without smoothing
-//         var wheelDelta = event.wheelDelta;
-//         var currentScrollPosition = window.pageYOffset;
-//         window.scrollTo(0, currentScrollPosition - wheelDelta);
-//     });
-// }
+        //scroll without smoothing
+        var wheelDelta = event.wheelDelta;
+        var currentScrollPosition = window.pageYOffset;
+        window.scrollTo(0, currentScrollPosition - wheelDelta);
+    });
+}
 
 // $(document).ready(function(){
 //     var $top
